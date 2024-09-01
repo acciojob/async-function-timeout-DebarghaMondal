d32b3f853e25951ac1c6cfbc892f12ab8176c5c0
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-09-01 09:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-document.getElementById('myForm').addEventListener('submit', async function(event){
-	event.preventDefault();
-
-	const text = document.getElementById('text').value;
-	const delayMs = parseInt(document.getElementById('delay').value*1000, 10);
-
-	if(isNaN(delayMs) || delayMs < 0){
-		alert('Please enter a valid positive number for delay.');
-		return;
-	}
-
-	await delay(delayMs);
-
-	document.getElementById('output').textContent = text;
-});
 
 
